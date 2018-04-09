26fed555d74eb4cbff99a2c5a9e6484a4a3a3370
KyraB <kyra.belanger@yahoo.com>
2018-04-09 12:56:10 -0500

    added 2 more java files

---
+++ app/src/main/java/com/example/kb974609/mystore/CreateTable.java
@@ -0,0 +1,10 @@
+package com.example.kb974609.mystore;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Kyra on 4/9/18.
+ */
+
+public class CreateTable extends AppCompatActivity {
+}
