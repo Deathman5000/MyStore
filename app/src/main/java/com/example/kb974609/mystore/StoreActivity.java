5ead970c6aadfaf18bdcb327cff55aadf8c4d80b
KyraB <kyra.belanger@yahoo.com>
2018-04-09 12:55:37 -0500

    added to store activity and created more java files

--- app/src/main/java/com/example/kb974609/mystore/StoreActivity.java
+++ app/src/main/java/com/example/kb974609/mystore/StoreActivity.java
@@ -1,5 +1,6 @@
 package com.example.kb974609.mystore;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,11 +12,22 @@ public class StoreActivity extends AppCompatActivity {
     ImageButton dasani, oatmeal, hotcakes, sausageBiscuit, baconEggBiscuit,
             eggSausageBiscuit, sausageBurrito;
     Button process;
+    int arrayCount;
+    //int dcount, ocount, hcount, scount, bcount, ecount, sbcount;
+    public static String[] products;
+    public static Double[] value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+
+        //dcount = ocount = hcount = scount = bcount = ecount = sbcount = 0;
+        arrayCount = 0;
+        products = new String[]{"Dasani Water ", "Fruit Maple Oatmeal ", "Hotcakes ",
+                "Sausage Biscuit ", "Bacon Egg Biscuit ", "Egg Sausage Biscuit ",
+                "Sausage Burrito "};
+        value = new Double[]{2.00, 2.00, 2.00, 1.99, 2.00, 2.00, 1.75};
 
         dasani = findViewById(R.id.DasaniButton);
         oatmeal = findViewById(R.id.FruitButton);
@@ -29,49 +41,84 @@ public class StoreActivity extends AppCompatActivity {
         dasani.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                arrayCount = 0;
+                Intent intent=new Intent(getApplicationContext(), CreateTable.class);
+                intent.putExtra("name", products[arrayCount]);
+                intent.putExtra("itemPrice", value[arrayCount]);
+                intent.putExtra("arrayCount", arrayCount);
+                startActivity(intent);
             }
         });
 
         oatmeal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                arrayCount = 1;
+                Intent intent=new Intent(getApplicationContext(), CreateTable.class);
+                intent.putExtra("name", products[arrayCount]);
+                intent.putExtra("itemPrice", value[arrayCount]);
+                intent.putExtra("arrayCount", arrayCount);
+                startActivity(intent);
             }
         });
 
         hotcakes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                arrayCount = 2;
+                Intent intent=new Intent(getApplicationContext(), CreateTable.class);
+                intent.putExtra("name", products[arrayCount]);
+                intent.putExtra("itemPrice", value[arrayCount]);
+                intent.putExtra("arrayCount", arrayCount);
+                startActivity(intent);
             }
         });
 
         sausageBiscuit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                arrayCount = 3;
+                Intent intent=new Intent(getApplicationContext(), CreateTable.class);
+                intent.putExtra("name", products[arrayCount]);
+                intent.putExtra("itemPrice", value[arrayCount]);
+                intent.putExtra("arrayCount", arrayCount);
+                startActivity(intent);
             }
         });
 
         baconEggBiscuit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                arrayCount = 4;
+                Intent intent=new Intent(getApplicationContext(), CreateTable.class);
+                intent.putExtra("name", products[arrayCount]);
+                intent.putExtra("itemPrice", value[arrayCount]);
+                intent.putExtra("arrayCount", arrayCount);
+                startActivity(intent);
             }
         });
 
         eggSausageBiscuit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                arrayCount = 5;
+                Intent intent=new Intent(getApplicationContext(), CreateTable.class);
+                intent.putExtra("name", products[arrayCount]);
+                intent.putExtra("itemPrice", value[arrayCount]);
+                intent.putExtra("arrayCount", arrayCount);
+                startActivity(intent);
             }
         });
 
         sausageBurrito.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                arrayCount = 6;
+                Intent intent=new Intent(getApplicationContext(), CreateTable.class);
+                intent.putExtra("name", products[arrayCount]);
+                intent.putExtra("itemPrice", value[arrayCount]);
+                intent.putExtra("arrayCount", arrayCount);
+                startActivity(intent);
             }
         });
 
