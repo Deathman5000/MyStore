6488ffe7d4ede14d293ba9fd59e3c7c8f76de48b
KyraB <kyra.belanger@yahoo.com>
2018-04-09 10:36:43 -0500

    Process Order button added and all onclick started

--- app/src/main/java/com/example/kb974609/mystore/StoreActivity.java
+++ app/src/main/java/com/example/kb974609/mystore/StoreActivity.java
@@ -2,12 +2,15 @@ package com.example.kb974609.mystore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class StoreActivity extends AppCompatActivity {
 
     ImageButton dasani, oatmeal, hotcakes, sausageBiscuit, baconEggBiscuit,
             eggSausageBiscuit, sausageBurrito;
+    Button process;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +23,63 @@ public class StoreActivity extends AppCompatActivity {
         sausageBiscuit = findViewById(R.id.SausageBiscuitButton);
         baconEggBiscuit = findViewById(R.id.BaconBiscuitButton);
         eggSausageBiscuit = findViewById(R.id.EggSausageButton);
-        sausageBiscuit = findViewById(R.id.SausageBurritoButton);
+        sausageBurrito = findViewById(R.id.SausageBurritoButton);
+        process = findViewById(R.id.ProcessButton);
 
+        dasani.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+        oatmeal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        hotcakes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        sausageBiscuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        baconEggBiscuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        eggSausageBiscuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        sausageBurrito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        process.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
